b0912b916f77298b8040f266d7a7075ba074aca8
Khalid Sanggoyod <khalid.sanggoyod@fligno.com>
2023-05-25 12:34:24 +0800

    Working on create user

--- src/service/auth.service.ts
+++ src/service/auth.service.ts
@@ -1,7 +1,18 @@
 import { Request, Response, NextFunction } from "express";
+import { User } from "../model/user.model";
+import { UserInterface } from "../lib/interface/auth/index.interface";
 
 export async function signUp(req: Request, res: Response, next: NextFunction) {
-  res.send("Signup Successfully");
+  const payload: UserInterface = req.body
+
+  let user = new User(payload);
+  user.save();
+
+  res.status(200).json({
+    success: true,
+    message: "Successfully created user",
+    user
+  })
 }
 
 export async function signIn(req: Request, res: Response, next: NextFunction) {
